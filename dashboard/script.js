4359146dda00c4c222ad500e76412e78a6a785fb
Prabhat kumar gupta <guptajim3636@gmail.com>
2023-01-20 23:18:44 +0530

    day4 updated

---
+++ dashboard/script.js
@@ -0,0 +1,109 @@
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=> {
+	const li = item.parentElement;
+
+	item.addEventListener('click', function () {
+		allSideMenu.forEach(i=> {
+			i.parentElement.classList.remove('active');
+		})
+		li.classList.add('active');
+	})
+});
+
+
+
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+	sidebar.classList.toggle('hide');
+})
+
+
+
+
+
+
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function (e) {
+	if(window.innerWidth < 576) {
+		e.preventDefault();
+		searchForm.classList.toggle('show');
+		if(searchForm.classList.contains('show')) {
+			searchButtonIcon.classList.replace('bx-search', 'bx-x');
+		} else {
+			searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		}
+	}
+})
+
+
+
+
+
+if(window.innerWidth < 768) {
+	sidebar.classList.add('hide');
+} else if(window.innerWidth > 576) {
+	searchButtonIcon.classList.replace('bx-x', 'bx-search');
+	searchForm.classList.remove('show');
+}
+
+
+window.addEventListener('resize', function () {
+	if(this.innerWidth > 576) {
+		searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		searchForm.classList.remove('show');
+	}
+})
+
+
+
+const switchMode = document.getElementById('switch-mode');
+
+switchMode.addEventListener('change', function () {
+	if(this.checked) {
+		document.body.classList.add('dark');
+	} else {
+		document.body.classList.remove('dark');
+	}
+})
+
+
+
+fetch("https://user2-api.onrender.com/womensdata")
+.then((res)=>{
+    return res.json();
+})
+.then((ans)=>{
+console.log(ans);
+    total(ans)
+     let note=-Infinity;
+  ans.map(element=>{
+  if(element.id>note)
+  {
+    note=element.id;
+  }
+  })
+  totalpro(note)
+})
+
+
+function totalpro(note){
+let totalproduct=document.getElementById("totalproduct");
+totalproduct.append(note);
+}
+
+function total(ans){
+    let sum=0;
+    ans.map(element=>{
+       sum+=element.Price;
+        })
+        console.log(sum);
+}
