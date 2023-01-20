904fde399c63c78c8eea9c123dfc62f13b1738cd
KamleshRahane <kamleshrahane999@gmail.com>
2023-01-20 21:30:46 +0530

    Day-4 Task Kamlesh

--- login.js
+++ login.js
@@ -1,9 +1,4 @@
-// $(".input_text").focus(function(){
-//     $(this).prev('.fa').addclass('glowIcon')
-// })
-// $(".input_text").focusout(function(){
-//     $(this).prev('.fa').removeclass('glowIcon')
-// })
+
 
 
   let form = document.querySelector("#login_button>a");
