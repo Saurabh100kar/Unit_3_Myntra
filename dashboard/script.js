599e567a4f972160dfb0ccabb7287915af86880b
Prabhat kumar gupta <guptajim3636@gmail.com>
2023-01-21 23:38:35 +0530

    done

--- dashboard/script.js
+++ dashboard/script.js
@@ -75,35 +75,107 @@ switchMode.addEventListener('change', function () {
 	}
 })
 
+let fullTotal= 0;
+let fullexp=0;
 
-
-fetch("https://user2-api.onrender.com/womensdata")
+fetch("https://wooomapi.onrender.com/womensdata")
+// fetch("https://mensdataa.onrender.com/mensdata")
 .then((res)=>{
     return res.json();
 })
 .then((ans)=>{
-console.log(ans);
-    total(ans)
-     let note=-Infinity;
-  ans.map(element=>{
-  if(element.id>note)
-  {
-    note=element.id;
-  }
-  })
-  totalpro(note)
+    sum(ans)
+    totalproduct(ans)
 })
 
+let totalinvest=document.getElementById("totalinvest")
 
-function totalpro(note){
-let totalproduct=document.getElementById("totalproduct");
-totalproduct.append(note);
+function sum(data){
+    let s=0;
+    data.map((Element)=>{
+        s+=Element.Price;
+        // console.log(typeof Element.Price)
+    })
+    fullexp=s;
+	// totalinvest.append(s)
+    console.log(fullexp)
 }
 
-function total(ans){
-    let sum=0;
-    ans.map(element=>{
-       sum+=element.Price;
-        })
-        console.log(sum);
+// let totalproduct=document.getElementById("totalproduct");
+
+
+function totalproduct(data){
+    let count1=0;
+    for(let i=0;i<data.length;i++){
+        count1++;
+    }
+	fullTotal = count1;
+	console.log(fullTotal)
+    // totalproduct.append(count);
+
 }
+
+
+//---------------------womensdatafetch--------------------------------------------------------------------
+fetch("https://mensdataa.onrender.com/mensdata")
+.then((res)=>{
+    return res.json();
+})
+.then((ans)=>{
+    ss(ans)
+    totalwomensproduct(ans)
+})
+
+function totalwomensproduct(data){
+    let count2=0;
+    for(let i=0;i<data.length;i++){
+        count2++;
+    }
+
+	fullTotal += count2;
+	console.log(fullTotal)
+	    let totalproduct=document.getElementById("totalproduct");
+    totalproduct.append(fullTotal);
+
+}
+
+function ss(data){
+    let sss=0;
+    data.map((Element)=>{
+        sss+=Element.Price;
+        // console.log(typeof Element.Price)
+    })
+	console.log(sss)
+    fullexp+=sss;
+	let totalprice=document.getElementById("totalinvest");
+	totalprice.append(fullexp)
+    console.log(fullexp)
+}
+
+
+// .then((ans)=>{
+// console.log(ans);
+    
+//      let note=-Infinity;
+//   ans.map(element=>{
+//   if(element.id>note)
+//   {
+//     note=element.id;
+//   }
+//   })
+//   totalpro(note)
+// })
+
+
+// function totalpro(note){
+// let totalproduct=document.getElementById("totalproduct");
+// totalproduct.append(note);
+// }
+
+// function total(ans){
+//     let sum=0;
+//     ans.map(element=>{
+//        sum+=element.Price;
+//         })
+//         console.log(sum);
+// }
